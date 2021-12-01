ce7d544889aeca33e09d31921459dc25533b9b04
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-01 11:48:55 -0500

    Import route and add route logic for Tasks route in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      
+      <h1>Lets Get Organized!</h1>
+      <Switch>
+        <Route path="/tasks" component={TasksContainer} />
+
+      </Switch>
     </div>
   );
 }
