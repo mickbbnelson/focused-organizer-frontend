e957eba30473ddafb3600debc2fa037b80d3cb31
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-01 13:25:48 -0500

    Set up basic CalendarContainer component and route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import TasksContainer from './containers/TasksContainer';
+import CalendarContainer from './containers/CalendarContainer';
 import Home from './Home'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <h1>Lets Get Organized!</h1>
       <Switch>
         <Route path="/tasks" component={TasksContainer} />
+        <Route path="/calendar" component={CalendarContainer} />
         <Route path="/" component={Home} />
       </Switch>
     </div>
---
+++ src/containers/CalendarContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class CalendarContainer extends React.Component {
+  
+  
+    render() {
+    return <h1>Calendar Container</h1>;
+  }
+}
+
+export default CalendarContainer;
