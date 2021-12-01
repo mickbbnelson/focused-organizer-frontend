2387bd343398782af04226bab5215a78764e5bf1
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-01 13:14:17 -0500

    Add basic home component and route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import TasksContainer from './containers/TasksContainer';
+import Home from './Home'
 
 function App() {
   return (
     <div>
       <h1>Lets Get Organized!</h1>
       <Switch>
-        
         <Route path="/tasks" component={TasksContainer} />
+        <Route path="/" component={Home} />
       </Switch>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Home extends React.Component {
+  
+  
+    render() {
+    return <p>
+        Stay organize, keep focused, acheive your goals
+    </p>;
+  }
+}
+
+export default Home;
