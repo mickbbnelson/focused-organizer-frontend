9ae199a08b9b4dc608e18788d418e745f706bda3
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-01 14:22:19 -0500

    Add Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
-import Pages from './components/Pages'
+import Pages from './components/Pages';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div>
+      <NavBar />
       <h1>Lets Get Organized!</h1>
       <Pages />
     </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+export default function NavBar() {
+
+    return (
+        <div>
+            <NavLink to='tasks'>Tasks</NavLink>
+            <NavLink to='calendar'>Calendar</NavLink>
+            <NavLink to='/'>Home</NavLink>
+        </div>
+    )
+}
