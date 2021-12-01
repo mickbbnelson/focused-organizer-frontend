d85c02d9c518d6a40872bccbc53ccc92195623a5
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-01 13:01:22 -0500

    Add TasksContainer component start and route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
+import TasksContainer from './containers/TasksContainer';
 
 function App() {
   return (
     <div>
       <h1>Lets Get Organized!</h1>
       <Switch>
+        
         <Route path="/tasks" component={TasksContainer} />
-
       </Switch>
     </div>
   );
---
+++ src/containers/TasksContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class TasksContainer extends React.Component {
+  
+  
+    render() {
+    return <h1>Tasks Container</h1>;
+  }
+}
+
+export default TasksContainer;
