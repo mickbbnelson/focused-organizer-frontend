5091061b4bf45b6214c4356930ec9cdad54e65b8
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-02 15:17:52 -0500

    Add task form base

--- src/components/taskForm.js
+++ src/components/taskForm.js
@@ -4,7 +4,22 @@ class taskForm extends React.Component {
   
   
     render() {
-    return <h1>Add a Task</h1>;
+    return (
+    <div>
+        <h1>Add a Task</h1>
+        <form>
+            <label>Task:</label>
+            <input type="text" name="task" />
+            <label>Priority:</label>
+            <input type="text" name="priority" />
+            <label>Category:</label>
+            <input type="text" name="category" />
+            <label>Notes:</label>
+            <input type="text" name="notes" />
+            <input type="submit" value="Add Task"/>
+        </form>
+    </div>
+    );
   }
 }
 
