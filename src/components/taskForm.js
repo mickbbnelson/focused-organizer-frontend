61b42d5f79e40d3b987b868d96892c03ee2ac05b
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-02 16:07:10 -0500

    Implement htmlFor and id's for form

--- src/components/taskForm.js
+++ src/components/taskForm.js
@@ -8,14 +8,14 @@ class TaskForm extends React.Component {
     <div>
         <h1>Add a Task</h1>
         <form>
-            <label>Task:</label>
-            <input type="text" name="task" />
-            <label>Priority:</label>
-            <input type="text" name="priority" />
-            <label>Category:</label>
-            <input type="text" name="category" />
-            <label>Notes:</label>
-            <input type="text" name="notes" />
+            <label htmlFor="task-input">Task:</label>
+            <input type="text" name="task" id="task-input" />
+            <label htmlFor="priority-input">Priority:</label>
+            <input type="text" name="priority" id="priority-input" />
+            <label htmlFor="category-input">Category:</label>
+            <input type="text" name="category" id="category-input" />
+            <label htmlFor="notes-input">Notes:</label>
+            <input type="text" name="notes" id="notes-input" />
             <input type="submit" value="Add Task"/>
         </form>
     </div>
