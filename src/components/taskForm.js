0baff3d5a79db8f3ed35d9dfabf755fc44cd2eb3
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-03 18:43:37 -0500

    Add dropdowns to task form

--- src/components/taskForm.js
+++ src/components/taskForm.js
@@ -34,12 +34,24 @@ class TaskForm extends React.Component {
         <form onSubmit={this.handleSubmit}>
             <label htmlFor="task-input">Task:</label>
             <input type="text" name="task" id="task-input" onChange={this.handleChange} value={this.state.task} />
+            
             <label htmlFor="priority-input">Priority:</label>
-            <input type="text" name="priority" id="priority-input" onChange={this.handleChange} value={this.state.priority} />
+            <select type="text" name="priority" id="priority-input" onChange={this.handleChange}> 
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>           
+            </select>
+            
             <label htmlFor="category-input">Category:</label>
-            <input type="text" name="category" id="category-input" onChange={this.handleChange} value={this.state.category} />
+            <select type="text" name="category" id="category-input" onChange={this.handleChange}> 
+                <option value="Home">Home</option>
+                <option value="School">School</option>
+                <option value="Work">Work</option>  
+            </select>
+            
             <label htmlFor="notes-input">Notes:</label>
             <input type="text" name="notes" id="notes-input" onChange={this.handleChange} value={this.state.notes} />
+
             <input type="submit" value="Add Task"/>
         </form>
     </div>
