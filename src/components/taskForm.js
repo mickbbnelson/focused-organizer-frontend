e3185082ca142e543e89754d45bba6a1c5a29cb7
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-02 19:36:21 -0500

    Add values to form

--- src/components/taskForm.js
+++ src/components/taskForm.js
@@ -18,15 +18,15 @@ class TaskForm extends React.Component {
     return (
     <div>
         <h1>Add a Task</h1>
-        <form>
+        <form >
             <label htmlFor="task-input">Task:</label>
-            <input type="text" name="task" id="task-input" onChange={this.handleChange} />
+            <input type="text" name="task" id="task-input" onChange={this.handleChange} value={this.state.task} />
             <label htmlFor="priority-input">Priority:</label>
-            <input type="text" name="priority" id="priority-input" onChange={this.handleChange} />
+            <input type="text" name="priority" id="priority-input" onChange={this.handleChange} value={this.state.priority} />
             <label htmlFor="category-input">Category:</label>
-            <input type="text" name="category" id="category-input" onChange={this.handleChange} />
+            <input type="text" name="category" id="category-input" onChange={this.handleChange} value={this.state.category} />
             <label htmlFor="notes-input">Notes:</label>
-            <input type="text" name="notes" id="notes-input" onChange={this.handleChange} />
+            <input type="text" name="notes" id="notes-input" onChange={this.handleChange} value={this.state.notes} />
             <input type="submit" value="Add Task"/>
         </form>
     </div>
