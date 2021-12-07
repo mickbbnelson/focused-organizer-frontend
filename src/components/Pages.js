39e3922bbc70c80df22d25abc82edca33ea037a7
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-07 13:56:40 -0500

    Add temp tasks/tasks route

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -6,10 +6,12 @@ import Home from './Home'
 import TaskViewCard from './TaskViewCard'
 
 export default function Pages() {
+    //const taskRoutes = '/tasks' || 'tasks/tasks'
 
     return (
         <Switch>
             <Route exact path="/tasks" component={routerProps => <TasksContainer routerProps={routerProps} />} />
+            <Route exact path="/tasks/tasks" component={routerProps => <TasksContainer routerProps={routerProps} />} />
             <Route path="/calendar" component={routerProps => <CalendarContainer routerProps={routerProps} />} />
             <Route path="/tasks/:id" component={routerProps => <TaskViewCard routerProps={routerProps} />} />
             <Route exact path="/" component={routerProps => <Home routerProps={routerProps} />} />
