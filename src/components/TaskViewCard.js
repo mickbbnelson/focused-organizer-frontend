7b11a72886c56b4ed634150e44ea5bf25bc771bc
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-06 19:08:02 -0500

    Add tasks/:id/editt route to TaskViewCard

--- src/components/TaskViewCard.js
+++ src/components/TaskViewCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux"
 import EditTaskForm from "./EditTaskForm"
+import { Switch, Route, Link } from 'react-router-dom';
 
 
 class TaskViewCard extends React.Component {
@@ -20,15 +21,20 @@ class TaskViewCard extends React.Component {
     console.log(this.state)
     }
 
-
     render(){
+        const viewLink = `/tasks/${this.state.id}/edit`
         return (
             <div>
             <h1>{this.state.task}</h1> 
             <p>Priority: {this.state.priority}</p> 
             <p>Category: {this.state.category}</p> 
             <p>Notes: {this.state.notes}</p> 
-            <EditTaskForm task={this.state.task} priority={this.state.priority} category={this.state.category} notes={this.state.notes} id={this.state.id} />
+            <Switch>
+                <Route path="/tasks/:id/edit" component={routerProps => <EditTaskForm routerProps={routerProps} task={this.state.task} priority={this.state.priority} category={this.state.category} notes={this.state.notes} id={this.state.id}/>} />
+            </Switch>
+            <Link to={viewLink} >
+            <button>Edit</button>
+            </Link>
             </div>  
         )
     }
