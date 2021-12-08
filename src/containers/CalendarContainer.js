2824ac7c4d6cd989c8fdcef75e0acac67a37b3b3
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-08 18:33:16 -0500

    Add initial fullCalendar logic

--- src/containers/CalendarContainer.js
+++ src/containers/CalendarContainer.js
@@ -1,10 +1,18 @@
 import React from "react";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+//import interactionPlugin from '@fullcalendar/interaction'
+//import '../app.css'
 
 class CalendarContainer extends React.Component {
   
   
     render() {
-    return <h1>Calendar Container</h1>;
+    return (      
+    <FullCalendar
+      plugins={[ dayGridPlugin ]}
+      initialView="dayGridMonth"
+    />)
   }
 }
 
