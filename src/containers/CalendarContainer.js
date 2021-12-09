cb99de8b2724492771137cbd2b2f9278b0a4484e
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-08 19:42:02 -0500

    Add test events to calendar

--- src/containers/CalendarContainer.js
+++ src/containers/CalendarContainer.js
@@ -1,8 +1,7 @@
 import React from "react";
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-//import interactionPlugin from '@fullcalendar/interaction'
-//import '../app.css'
+import listPlugin from '@fullcalendar/list'
+import interactionPlugin from '@fullcalendar/interaction'
 
 class CalendarContainer extends React.Component {
   
@@ -10,8 +9,9 @@ class CalendarContainer extends React.Component {
     render() {
     return (      
     <FullCalendar
-      plugins={[ dayGridPlugin ]}
-      initialView="dayGridMonth"
+      plugins={[ listPlugin ]}
+      initialView="listWeek"
+      events={[{title: 'test', date: '2021-12-09T12:30:00'}, {title: 'test 2', date: '2021-12-10T03:00:00'}]}
     />)
   }
 }
