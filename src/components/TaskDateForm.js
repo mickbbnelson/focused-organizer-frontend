013e3482f9850134f2a53fb6a9241a29d2bb4998
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-09 10:50:48 -0500

    Add ISO conversion to TaskDateForm component

--- src/components/TaskDateForm.js
+++ src/components/TaskDateForm.js
@@ -6,20 +6,22 @@ class TaskDateForm extends React.Component {
     date: ''
   }
 
-
   handleChange = (event) => {
    this.setState({
        date: event.target.value
    })
   }
 
+  componentDidUpdate(){
+    let eventDate = new Date(this.state.date)
+    console.log(eventDate.toISOString());
+  }
+
     render() {
     return (
       <div>
-        <form>
         <label htmlFor="date-input">Date:</label>
             <input type="text" name="date" id="date-input" onChange={this.handleChange} value={this.state.date} />
-        </form>
       </div>
     );
   }
