286ab1d13fbfc82489789c087e87b381514d80f8
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-12-12 16:36:25 -0500

    Add CalendarCard component

---
+++ src/components/FullCalendarCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CalendarCard = (props) => {
+    console.log(props)
+    return (
+        <div id="calendar-card">
+        </div>
+    )
+}
+
+export default CalendarCard;
