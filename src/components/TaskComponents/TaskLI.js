3f8c3c81866124e59cce8571a1695d0e62001d72
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-01-06 17:23:46 -0600

    Add like function for code challenge

--- src/components/TaskComponents/TaskLI.js
+++ src/components/TaskComponents/TaskLI.js
@@ -1,18 +1,32 @@
 import { Link } from 'react-router-dom'
+import React from "react"
 
-const TaskLI = (props) => {
+class TaskLI extends React.Component {
 
-    const viewLink = `/tasks/${props.task.id}`
+    state = {
+        counter: 0
+    }
 
+    handleClick = () => {
+        this.setState(prevState => ({
+            counter: prevState.counter + 1
+           }))
+    }
+
+    render() {
+    const viewLink = `/tasks/${this.props.task.id}`
     return (
         <div id="task-card">
-            <span>{props.task.title} - Priority: {props.task.priority}</span>
-            <button onClick={() => props.handleDelete(props.task.id)} id="delete">Delete</button>
+            <span>{this.props.task.title} - Priority: {this.props.task.priority}</span>
+            <button onClick={() => this.props.handleDelete(this.props.task.id)} id="delete">Delete</button>
             <Link to={viewLink}>
             <button id="view">View</button>
             </Link>
+            <button onClick={this.handleClick}>Like</button>
+            <p>Likes:{this.state.counter}</p>
+            {console.log(this.state)}
         </div>
     )
-}
+}}
 
 export default TaskLI;
