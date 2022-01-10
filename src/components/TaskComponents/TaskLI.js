7f4de9cf4b776248d3bfcda11c1464fcead8cd40
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-01-10 15:54:59 -0600

    Update props in TaskLI component

--- src/components/TaskComponents/TaskLI.js
+++ src/components/TaskComponents/TaskLI.js
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 import React from "react"
 
-const TaskLI = () => {
-    const viewLink = `/tasks/${this.props.task.id}`
+const TaskLI = (props) => {
+    const viewLink = `/tasks/${props.task.id}`
     return (
         <div id="task-card">
-            <span>{this.props.task.title} - Priority: {this.props.task.priority}</span>
-            <button onClick={() => this.props.handleDelete(this.props.task.id)} id="delete">Delete</button>
+            <span>{props.task.title} - Priority: {props.task.priority}</span>
+            <button onClick={() => this.props.handleDelete(props.task.id)} id="delete">Delete</button>
             <Link to={viewLink}>
             <button id="view">View</button>
             </Link>
