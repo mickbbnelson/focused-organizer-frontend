8900bbd0681aabf8436502bd44fde7ae981a5bb2
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-01-10 17:06:57 -0600

    Update background Image

--- src/components/TaskComponents/TaskLI.js
+++ src/components/TaskComponents/TaskLI.js
@@ -6,7 +6,7 @@ const TaskLI = (props) => {
     return (
         <div id="task-card">
             <span>{props.task.title} - Priority: {props.task.priority}</span>
-            <button onClick={() => this.props.handleDelete(props.task.id)} id="delete">Delete</button>
+            <button onClick={() => props.handleDelete(props.task.id)} id="delete">Delete</button>
             <Link to={viewLink}>
             <button id="view">View</button>
             </Link>
