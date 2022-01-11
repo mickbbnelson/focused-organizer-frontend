ddf3f9ed6277c16bb0e04632864188138a239fb0
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-01-11 10:08:22 -0600

    Add CalendarChange Component

---
+++ src/components/CalendarComponents/CalendarChange.js
@@ -0,0 +1,21 @@
+const CalendarChange = (props) => {
+  
+    const calendarUpdate = (event) => {
+      event.preventDefault();
+      props.CalendarUpdate(event.target.value);
+    } 
+      
+        return (
+          <div className="center">
+            <form onChange={calendarUpdate}>
+              <label>Calendar View:</label>
+              <select type="text" name="category" id="category-input"> 
+                <option value="Week">Week</option>
+                <option value="Home">Home</option>
+              </select> 
+            </form>
+          </div>
+          )
+      }
+  
+  export default CalendarChange
--- src/containers/CalendarContainer.js
+++ src/containers/CalendarContainer.js
@@ -2,8 +2,13 @@ import React from "react"
 import {connect} from "react-redux"
 import {getTasks} from '../actions/TaskActions'
 import CalendarPage from '../components/CalendarComponents/CalendarPage'
+import CalendarChange from '../components/CalendarComponents/CalendarChange'
 
 class CalendarContainer extends React.Component {
+
+  state = {
+    view: ''
+  }
 
   componentDidMount(){
     this.props.dispatchTasks()
@@ -13,10 +18,14 @@ class CalendarContainer extends React.Component {
       let eventArray = this.props.tasks.map((task) => {
         return {title: task.title, date: task.date}
       }) 
-      return (     
-       <CalendarPage tasks={eventArray} />)
+      if (this.state.view === '' || 'week') {
+      return ( 
+       <> 
+       <CalendarChange />   
+       <CalendarPage tasks={eventArray} />
+       </>)
   }
-}
+}}
 
 function mapDispatchToProps(dispatch){
   return {
